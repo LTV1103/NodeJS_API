b963a42ef8cc387eda1ecfbfa0898f4fe5a8c764
LTV1103 <luongtrieuvy1103@gmail.com>
2025-03-31 23:39:56 +0700

    update

--- config/db.js
+++ config/db.js
@@ -1,33 +1,15 @@
+require("dotenv").config();
 const mysql = require("mysql2/promise");
 
-// Cấu hình kết nối
-// const connection = mysql.createConnection({
-//     host: "localhost", // Địa chỉ MySQL server
-//     user: "root",      // Tên đăng nhập MySQL
-//     password: "",      // Mật khẩu (để trống nếu dùng XAMPP)
-//     database: "ql_SucKhoe" // Tên database
-// });
-
-// // Kết nối tới MySQL
-// connection.connect((err) => {
-//     if (err) {
-//         console.error("Kết nối thất bại:", err);
-//         return;
-//     }
-//     console.log("Kết nối thành công đến MySQL!");
-// });
-
-// module.exports = connection;
-const MYSQL_URL =
-  "mysql -h turntable.proxy.rlwy.net -u root -p ntMHpXvdvBbzLLyPxptihIArpcCHIPaw --port 42986 --protocol=TCP railway";
 const pool = mysql.createPool({
-  uri: process.env.MYSQL_URL, // Biến môi trường MYSQL_URL từ Railway
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "ql_suckhoe",
+  host: process.env.MYSQL_PUBLIC_URL,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
-module.exports = pool; // Xuất pool hỗ trợ Promise
+
+module.exports = pool;
