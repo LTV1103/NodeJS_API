6d5544b2a2a4cbc68b6bc4584081c7eec22c54e2
LTV1103 <luongtrieuvy1103@gmail.com>
2025-04-08 21:28:55 +0700

    Update ChiSo.js

--- models/ChiSo.js
+++ models/ChiSo.js
@@ -30,7 +30,7 @@ class ChiSo {
   };
   static getchiso = async (ma) => {
     const sql =
-      "select ho_ten,gioi_tinh,chieu_cao_cm,can_nang_kg,huyet_ap,nhip_tim,BMI,ngay_do from chisosuckhoe join nguoidung on chisosuckhoe.ma_nguoi_dung = nguoidung.ma_nguoi_dung where chisosuckhoe.ma_nguoi_dung = ? ";
+      "select ma_chi_so,ho_ten,gioi_tinh,chieu_cao_cm,can_nang_kg,huyet_ap,nhip_tim,BMI,ngay_do from chisosuckhoe join nguoidung on chisosuckhoe.ma_nguoi_dung = nguoidung.ma_nguoi_dung where chisosuckhoe.ma_nguoi_dung = ? ";
     const [result] = await db.query(sql, [ma]);
     return result;
   };
